ff330a69b844b4581146e04b76acbdbfd036b946
amsterdam25 <75081421+ahmetamsterdamer@users.noreply.github.com>
2022-04-28 15:05:24 +0200

    Update Model.java
    I added customer ,employee, rental, and boat id there.

--- src/com/capgemini/model/Model.java
+++ src/com/capgemini/model/Model.java
@@ -1,4 +1,67 @@
 package com.capgemini.model;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Model {
+    public List<Boat> boats =  new ArrayList<>();
+    public List<Employee> employees = new ArrayList<>();
+    public List<Customer> customers = new ArrayList<>();
+    public List<Rental> rentals = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+    ObjectMapper mapper = new ObjectMapper();
+
+    public int nextBoatId(){
+        int id = 0;
+        for (Boat boatIn : boats ){
+            if (boatIn.getBoatId() > id){
+                id = boatIn.getBoatId();
+            }
+        }
+        id++;
+        return id;
+    }
+
+    public int nextCustomerId(){
+        int id = 0;
+        for (Customer customerIn : customers ){
+            if (customerIn.getCustomerId() > id){
+                id = customerIn.getCustomerId();
+            }
+        }
+        id++;
+        return id;
+    }
+
+    public int nextEmployeeId(){
+        int id = 0;
+        for (Employee employee : employees ){
+            if (employee.getEmployeeId() > id){
+                id = employee.getEmployeeId();
+            }
+        }
+        id++;
+        return id;
+    }
+
+    public int nextRentalId(){
+        int id = 0;
+        for (Rental rentalIn : rentals ){
+            if (rentalIn.getRentalId() > id){
+                id = rentalIn.getRentalId();
+            }
+        }
+        id++;
+        return id;
+    }
+
+    public static Model readModel() {
+        return null;
+    }
 }
+
